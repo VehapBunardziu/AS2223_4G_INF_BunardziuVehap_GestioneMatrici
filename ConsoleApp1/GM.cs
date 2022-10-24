98f281ae3849f4a661e2f137c431913a42e850ed
VehapBunardziu <vehap.bunardziu@studenti.ittsrimini.edu.it>
2022-10-24 11:45:51 +0200

    Iniziata la funzione StampaMatrice

--- ConsoleApp1/GM.cs
+++ ConsoleApp1/GM.cs
@@ -8,10 +8,20 @@ namespace AS2223_4G_INF_BunardziuVehap_GestioneMatrici
 {
     internal class GM
     {
+        private int lMat;
+        private int[,] mat;
+        private const int MAX_VALUE = -100;
+        private const int MIN_VALUE = 75;
 
         public void StampaMatrice()
         {
+            for(int i = 0; i < lMat; i++)
+            {
+                for(int j = 0; j < lMat; j++)
+                {
 
+                }
+            }
         }
         public void StampaCornice()
         {
@@ -32,3 +42,4 @@ namespace AS2223_4G_INF_BunardziuVehap_GestioneMatrici
         }
     }
 }
+
