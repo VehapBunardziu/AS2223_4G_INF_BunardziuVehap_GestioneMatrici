99fb7ad15570b7c9becd9a494cf47742c9274ef5
VehapBunardziu <vehap.bunardziu@studenti.ittsrimini.edu.it>
2022-10-24 11:16:47 +0200

    Aggiungere i file di progetto.

---
+++ ConsoleApp1/GM.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AS2223_4G_INF_BunardziuVehap_GestioneMatrici
+{
+    internal class GM
+    {
+
+        public void StampaMatrice()
+        {
+
+        }
+        public void StampaCornice()
+        {
+
+        }
+        public void CaricaMatrice()
+        {
+
+        }
+
+        public GM()
+        {
+
+        }
+        public void Determinante()
+        {
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AS2223_4G_INF_BunardziuVehap_GestioneMatrici // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }  
+    }
+}
